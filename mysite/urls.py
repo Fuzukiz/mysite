277f51582da8fc7da888c3f5c3ebbd3b7fc7fdc3
Fuzuki <temniakovslava@yandex.ru>
2021-06-09 17:43:38 +0500

    дз

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,4 @@
-"""notes URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,12 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
-from django.views.generic import RedirectView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='/catalog/', permanent=True))
+    path('', include('user_profile.urls')),
+    path('', include('frontend.urls')),
 ]
